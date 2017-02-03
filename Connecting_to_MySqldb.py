0109742245fbcbb9b58164157165cbe8f2b27542
bharathikambli <bharathi.kambli@asmltd.com>
2017-02-03 18:44:30 +0530

    Add files via upload

---
+++ Connecting_to_MySqldb.py
@@ -0,0 +1,18 @@
+#Connecting to MySql database
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","root123","EMP" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print ("Database version : %s " % data)
+
+# disconnect from server
+db.close()
